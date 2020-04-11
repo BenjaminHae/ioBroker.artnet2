36c3db0b84638481cd0e3df72aefb663ff8a11b8
Benjamin Häublein <benjaminh@debian.vm.hp>
2020-04-11 20:01:14 +0200

    empty iobroker adapter

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'artnet2 adapter settings': {
+        'en': 'Adapter settings for artnet2',
+        'de': 'Adaptereinstellungen für artnet2',
+        'ru': 'Настройки адаптера для artnet2',
+        'pt': 'Configurações do adaptador para artnet2',
+        'nl': 'Adapterinstellingen voor artnet2',
+        'fr': "Paramètres d'adaptateur pour artnet2",
+        'it': "Impostazioni dell'adattatore per artnet2",
+        'es': 'Ajustes del adaptador para artnet2',
+        'pl': 'Ustawienia adaptera dla artnet2',
+        'zh-cn': 'artnet2的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
