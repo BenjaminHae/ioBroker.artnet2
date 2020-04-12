76e508b58142fa53124c314487350aa872f8fb33
Benjamin Häublein <benjaminh@debian.vm.hp>
2020-04-12 11:18:43 +0200

    fix linting

--- src/lib/artnetController.ts
+++ src/lib/artnetController.ts
@@ -1,111 +1,120 @@
-class Channel {
-  channel: number;
-  currentValue: number;
+import * as Artnet from 'artnet';
 
-  constructor (channel: number, currentValue: number) {
-    this.channel = channel;
-    this.currentValue = Math.round(currentValue);
-  }
+class Channel {
+    channel: number;
+    currentValue: number;
+
+    constructor (channel: number, currentValue: number) {
+        this.channel = channel;
+        this.currentValue = Math.round(currentValue);
+    }
 }
 
 class DesiredState extends Channel {
-  desiredValue: number;
-  transitionPosition: number;
-  transitionLength: number;
+    desiredValue: number;
+    startValue: number;
+    transitionPosition: number;
+    transitionLength: number;
 
-  constructor (channel: number, desiredValue: number, transitionLength:number, currentValue: number) {
-    super(channel, currentValue);
-    this.desiredValue = Math.round(desiredValue);
-    this.transitionLength = Math.round(transitionLength);
-    this.transitionPosition = 0;
-  }
+    constructor (channel: number, desiredValue: number, transitionLength: number, currentValue: number) {
+        super(channel, currentValue);
+        this.startValue = this.currentValue;
+        this.desiredValue = Math.round(desiredValue);
+        this.transitionLength = Math.round(transitionLength);
+        this.transitionPosition = 0;
+    }
 
-  transition():void {
-    if (this.transitionPosition < this.transitionLength) {
-      this.currentValue = Math.round(this.currentValue + (this.desiredValue - this.currentValue) / (this.transitionLength - this.transitionPosition));
+    transition(): void {
+        if (this.transitionPosition < this.transitionLength) {
+            this.currentValue = Math.round(this.startValue + this.transitionPosition * (this.desiredValue - this.startValue) / this.transitionLength);
+        }
+        this.transitionPosition += 1;
+        if (this.transitionPosition >= this.transitionLength) {
+            this.currentValue = this.desiredValue;
+        }
     }
-    this.transitionPosition += 1;
-    if (this.transitionPosition >= this.transitionLength) {
-      this.currentValue = this.desiredValue;
+
+    achieved(): boolean {
+        return this.transitionPosition >= this.transitionLength;
     }
-  }
 }
 
 export class ArtnetController {
-  artnet: any;
-  universe: number;
-  transitions: DesiredState[];
-  loop: NodeJS.Timeout = null;
-
-  constructor (host: string, port: number, universe:number) {
-    this.artnet = require('artnet')({host: host, port: port || 6454, sendAll: true});
-    this.universe = universe;
-    this.transitions = [];
-  }
-
-  close (): void {
-    if (this.artnet) {
-      this.artnet.close();
-    }
-  }
-
-  setValue(channel: number, value: number, transitionLength: number = 0, currentValue: number = 0) {
-    let state = new DesiredState(channel, value, transitionLength, currentValue);
-    this.transitions.push(state);
+    artnet: any;
+    universe: number;
+    transitions: DesiredState[];
+    loop: NodeJS.Timeout = null;
+    periodLength: number;
     
-    this.initLoop();
-  }
 
-  initLoop() {
-    if (! this.loop) {
-      this.loop = setInterval(() => this.stateLoop(), 40);
-    }
-  }
-  
-  stopLoop() {
-    if (this.loop) {
-      clearInterval(this.loop);
-      this.loop = null;
-    }
-  }
-
-  sendToArtnet(channels: Array<Channel>): void {
-    channels.sort( (a:Channel, b:Channel) => { return a.channel - b.channel });
-    let startAddress: number = channels[0].channel;
-    let currentAddress: number = startAddress;
-    let values: Array<number> = [];
-    for (let item of channels) {
-      while (currentAddress < item.channel) {
-        values.push(null);
-        currentAddress += 1;
-      }
-      values.push(item.currentValue);
-      currentAddress += 1;
-    }
-    this.artnet.set(this.universe, startAddress, values);
-  }
-
-  stateLoop(): void {
-    if (this.transitions.length) {
-      // calculate new channel-values
-      for (let state of this.transitions) {
-        state.transition();
-      }
-
-      // send values to artnet
-      this.sendToArtnet(this.transitions);
-
-      // remove values that have been achieved
-      function filter (state: DesiredState) {
-        return state.transitionPosition < state.transitionLength;
-      }
-      this.transitions = this.transitions.filter ( filter );
+    constructor (host: string, port: number, universe: number, periodLength = 40) {
+        this.artnet = Artnet({host: host, port: port || 6454, sendAll: true});
+        this.universe = universe;
+        this.periodLength = periodLength;
+        this.transitions = [];
     }
 
-    // kill loop if all transitions are done
-    if (this.transitions.length == 0) {
-      this.stopLoop();
+    close (): void {
+        this.stopLoop();
+        if (this.artnet) {
+            this.artnet.close();
+        }
     }
-  }
+
+    setValue(channel: number, value: number, transitionLength = 0, currentValue = 0): void {
+        const state = new DesiredState(channel, value, transitionLength, currentValue);
+        this.transitions.push(state);
+        
+        this.initLoop();
+    }
+
+    initLoop(): void {
+        if (! this.loop) {
+            this.loop = setInterval(() => this.stateLoop(), this.periodLength);
+        }
+    }
+    
+    stopLoop(): void {
+        if (this.loop) {
+            clearInterval(this.loop);
+            this.loop = null;
+        }
+    }
+
+    sendToArtnet(channels: Array<Channel>): void {
+        channels.sort( (a: Channel, b: Channel) => { return a.channel - b.channel });
+        const startAddress: number = channels[0].channel;
+        let currentAddress: number = startAddress;
+        const values: Array<number> = [];
+        for (const item of channels) {
+            while (currentAddress < item.channel) {
+                values.push(null);
+                currentAddress += 1;
+            }
+            values.push(item.currentValue);
+            currentAddress += 1;
+        }
+        this.artnet.set(this.universe, startAddress, values);
+    }
+
+    stateLoop(): void {
+        if (this.transitions.length) {
+            // calculate new channel-values
+            for (const state of this.transitions) {
+                state.transition();
+            }
+
+            // send values to artnet
+            this.sendToArtnet(this.transitions);
+
+            // remove values that have been achieved
+            this.transitions = this.transitions.filter ( (state: DesiredState) => !state.achieved() );
+        }
+
+        // kill loop if all transitions are done
+        if (this.transitions.length == 0) {
+            this.stopLoop();
+        }
+    }
 }
 
