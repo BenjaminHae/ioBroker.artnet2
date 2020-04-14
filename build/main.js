5b31158ddc2f3632b4499854f4da498a7cbc844e
Benjamin Häublein <benjaminh@debian.vm.hp>
2020-04-14 20:26:12 +0200

    fix check for color

--- build/main.js
+++ build/main.js
@@ -156,7 +156,7 @@ class Artnet2 extends utils.Adapter {
                 this.artnetController.setValue(channel, state.val, transition, oldValue);
                 let stateName = id.split('.').pop();
                 this.log.info(`${stateName} may change rgb`);
-                if (stateName && stateName in ["red", "green", "blue"]) {
+                if (stateName && ["red", "green", "blue"].includes(stateName)) {
                     let color = this.genRgbColor(baseId);
                     this.log.info(`new Rgb: ${color}`);
                     this.setState(baseId + '.rgb', color);
--- src/main.ts
+++ src/main.ts
@@ -181,7 +181,7 @@ class Artnet2 extends utils.Adapter {
                 
                 let stateName = id.split('.').pop();
                 this.log.info(`${stateName} may change rgb`);
-                if (stateName && stateName in ["red","green","blue"]) {
+                if (stateName && ["red","green","blue"].includes(stateName)) {
                     let color = this.genRgbColor(baseId);
                     this.log.info(`new Rgb: ${color}`);
                     this.setState(baseId + '.rgb', color);
